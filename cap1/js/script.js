7294be6cd76a7ceda95595a82d8141c6e30d1b88
Juan Daniel Ferreira Ibañez <juan.ibanezfl@gmail.com>
2020-07-16 17:14:25 -0300

    6.3
    Atualizando e brincando um pouco com for, while, do while, e funções.

--- cap1/js/script.js
+++ cap1/js/script.js
@@ -1,16 +1,6 @@
 var a = 5;
 var b = 4;
 
-if (a > b) {
-  console.log(a + ' é maior que ' + b);
-} else {
-  if (a < b) {
-    console.log(a + ' é menor que ' + b);
-  } else {
-    console.log(a + 'é igual que ' + b);
-  }
-}
-
 var dia = 1;
 
 if (dia === 1) {
@@ -70,3 +60,66 @@ switch (dia) {
     r = 'Dia inválido';
 }
 console.log(r);
+
+// Operador ternário
+a = 6;
+b = 7;
+
+if (a > b) {
+  console.log(a + ' é maior que ' + b);
+} else {
+  if (a < b) {
+    console.log(a + ' é menor que ' + b);
+  } else {
+    console.log(a + 'é igual que ' + b);
+  }
+}
+
+var resposta = a > b ? 'maior' : a < b ? 'menor' : 'igual';
+console.log(resposta);
+
+var diaSemana =
+  dia === 1
+    ? 'Domingo'
+    : dia === 2
+    ? 'Segunda'
+    : dia === 3
+    ? 'terça'
+    : dia === 4
+    ? 'quarta'
+    : dia === 5
+    ? 'quinta'
+    : dia === 6
+    ? 'sexta'
+    : dia === 7
+    ? 'sabado'
+    : 'Dia iválido';
+console.log(diaSemana);
+
+// Somatório com while
+
+var numeroAtual = 1;
+var somatorio = 0;
+
+while (numeroAtual <= 10) {
+  //somatorio = somatorio + numeroAtual;
+  somatorio += numeroAtual;
+  numeroAtual++;
+}
+console.log('A soma é ' + somatorio);
+
+var numeroAtual = 1;
+var somatorio = 0;
+
+do {
+  somatorio += numeroAtual;
+  numeroAtual++;
+} while (numeroAtual <= 10);
+console.log('A soma é ' + somatorio);
+
+var somatorio = 0;
+
+for (numeroAtual = 1; numeroAtual <= 10; numeroAtual++) {
+  somatorio += numeroAtual;
+}
+console.log('A soma é ' + somatorio);
