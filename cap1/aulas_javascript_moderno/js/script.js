b80e69b8595a3ad92ffdf61c5a883210512e90c2
Juan Daniel Ferreira Ibañez <juan.ibanezfl@gmail.com>
2020-07-24 00:06:26 -0300

    Rest/spread
    Destructuring

--- cap1/aulas_javascript_moderno/js/script.js
+++ cap1/aulas_javascript_moderno/js/script.js
@@ -1,81 +1,36 @@
 window.addEventListener('load', () => {
-  doMap();
-  doFilter();
-  doForEach();
-  doReduce();
-  doFind();
-  doSome();
-  doEvery();
-  doSort();
+  doSpred();
+  doRest();
+  doDestructuring();
 });
-function doMap() {
-  const nameEmailArray = people.results.map((person) => {
-    return { name: person.name, email: person.email };
-  });
-  console.log(nameEmailArray);
+function doSpred() {
+  const marriedMen = people.results.filter(
+    (person) => person.name.title === 'Mr'
+  );
+  const marriedWomen = people.results.filter(
+    (person) => person.name.title === 'Ms'
+  );
+  const marriePeople = [...marriedMen, ...marriedWomen, { msg: 'oi' }];
+  console.log(marriePeople);
+}
 
-  return nameEmailArray;
+function doRest() {
+  console.log(infiniteSum(1, 2));
+  console.log(infiniteSum(1, 2, 1000));
+  console.log(infiniteSum(1, 2, 1000, 1, 2, 3, 4, 34, 34, 34, 34, 34, 2, 23));
 }
-function doFilter() {
-  const olderThan18 = people.results.filter((person) => {
-    return person.dob.age > 50;
-  });
-  console.log(olderThan18);
+function infiniteSum(...numbers) {
+  return numbers.reduce((acc, curr) => acc + curr, 0);
 }
-function doForEach() {
-  const mappedPeople = doMap();
+function doDestructuring() {
+  const first = people.results[0];
 
-  mappedPeople.forEach((person) => {
-    person.nameSize =
-      person.name.title.length +
-      person.name.first.length +
-      person.name.last.length;
-  });
-  console.log(mappedPeople);
-}
-function doReduce() {
-  const totalAges = people.results.reduce((accumulator, current) => {
-    return accumulator + current.dob.age;
-  }, 0);
-  console.log(totalAges);
+  //Repetitiva
+  //const username = first.login.username;
+  //const password = first.login.password;
 
-  //   let sumAges = 0;
-  //   for (let i = 0; i < people.results.length; i++) {
-  //     var current = people.results[i];
-  //     sumAges += current.dob.age;
-  //   }
-  //   console.log(sumAges);
-  //
-}
-function doFind() {
-  const found = people.results.find((person) => {
-    return person.location.state === 'Mato Grosso do Sul';
-  });
-  console.log(found);
-}
-function doSome() {
-  const found = people.results.some((person) => {
-    return person.location.state === 'Mato Grosso do Sul';
-  });
-  console.log(found);
-}
-function doEvery() {
-  const every = people.results.every((person) => {
-    return person.nat === 'BR';
-  });
-  console.log(every);
-}
-function doSort() {
-  const mappedNames = people.results
-    .map((person) => {
-      return {
-        name: person.name.first,
-      };
-    })
-    .filter((person) => person.name.startsWith('A'))
-    .sort((a, b) => {
-      return b.name.length - a.name.length;
-      // return a.name.localeCompare(b.name);
-    });
-  console.log(mappedNames);
+  //Usando destructuring
+  const { username, password } = first.login;
+  console.log(username);
+  console.log(password);
 }
